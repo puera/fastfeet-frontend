88ff44d0732296ab47c9dd088dd6dbf221e69a37
Renann Souza <renann_puera@hotmail.com>
2020-02-27 00:30:56 -0300

    Add Routes / pages / history service

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '~/pages/SignIn';
+
+import Dashboard from '~/pages/Dashboard';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/dashboard" component={Dashboard} />
+    </Switch>
+  );
+}
