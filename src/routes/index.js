404cfbcd7b51e34d22a5672e05324449e23ba550
Renann Souza <renann_puera@hotmail.com>
2020-02-27 00:51:50 -0300

    Add Custom Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 
@@ -10,7 +11,7 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
 
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
