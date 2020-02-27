43d35f9f178b23b7f5263f55ac2e69ef6d1b1d8a
Renann Souza <renann_puera@hotmail.com>
2020-02-27 01:01:01 -0300

    Add Custom Layout pages

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '~/pages/_layout/auth';
+import DefaultLayout from '~/pages/_layout/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
@@ -16,8 +19,18 @@ export default function RouteWrapper({
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
+  const Layout = signed ? DefaultLayout : AuthLayout;
 
-  return <Route {...rest} component={Component} />;
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
